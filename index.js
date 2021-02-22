08d65d99d52159281803fd92304c3c475c837e33
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-02-22 11:21:41 +0000

    added createBankController functionality

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ const server = express();
 server.use(bodyParser.json());
 
 
-const banks = []
+const banksDB = []
+
+
+class BankModel {
+    constructor({name, location, branch, phone, address, accountNumber}){
+        this.name = name
+        this.location = location
+        this.branch = branch
+        this.phone = phone
+        this.address = address
+        this.accountNumber = accountNumber
+    }
+
+    save(){
+        banksDB.push(this)
+        return this
+    }
+}
  
 
 
@@ -16,7 +33,13 @@ const viewBanksController = (req, res) => {
 }
 
 const createBankController = (req, res) => {
+    const {name, location, branch, phone, address, accountNumber} = req.body
 
+    const bank = BankModel({name, location, branch, phone, address, accountNumber})
+
+    bank.save()
+
+    res.json({message: "create successful", data: bank})
 }
 
 const updateBankController = (req, res) => {
