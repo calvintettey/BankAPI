c26714782455a3ab3dd89c546adba52045ef36b1
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-02-22 11:53:16 +0000

    added updateBankController functionality

--- index.js
+++ index.js
@@ -6,53 +6,80 @@ const server = express();
 
 server.use(bodyParser.json());
 
-
-const banksDB = []
-
+let banksDB = [];
 
 class BankModel {
-    constructor({name, location, branch, phone, address, accountNumber}){
-        this.name = name
-        this.location = location
-        this.branch = branch
-        this.phone = phone
-        this.address = address
-        this.accountNumber = accountNumber
-    }
+  constructor({ name, location, branch, phone, address, accountNumber }) {
+    this.name = name;
+    this.location = location;
+    this.branch = branch;
+    this.phone = phone;
+    this.address = address;
+    this.accountNumber = accountNumber;
+  }
 
-    save(){
-        banksDB.push(this)
-        return this
-    }
+  save() {
+    banksDB.push(this);
+    return this;
+  }
+
+  static all() {
+    return banksDB;
+  }
+
+  static update(updatedInfo = {}) {
+    banksDB.map((bank) => {
+      if (bank.name === updatedInfo.name) {
+        return { ...bank, ...updatedInfo };
+      }
+
+      return bank;
+    });
+  }
 }
- 
-
 
 const viewBanksController = (req, res) => {
-
-}
+  const banks = BankModel.all();
+  res.json({ data: banks });
+};
 
 const createBankController = (req, res) => {
-    const {name, location, branch, phone, address, accountNumber} = req.body
+  const { name, location, branch, phone, address, accountNumber } = req.body;
 
-    const bank = BankModel({name, location, branch, phone, address, accountNumber})
+  const bank = new BankModel({
+    name,
+    location,
+    branch,
+    phone,
+    address,
+    accountNumber,
+  });
 
-    bank.save()
+  bank.save();
 
-    res.json({message: "create successful", data: bank})
-}
+  res.json({ message: "create successful", data: bank });
+};
 
 const updateBankController = (req, res) => {
+  const { name, location, branch, phone, address, accountNumber } = req.body;
 
-}
+  const updatedBank = BankModel.update({
+    name,
+    location,
+    branch,
+    phone,
+    address,
+    accountNumber,
+  });
 
-const deleteBankController = (req, res) => {
+  res.json({ maessage: "update successful", data: updatedBank });
+};
 
-}
+const deleteBankController = (req, res) => {};
 
-server.get('/bank', viewBanksController)
-server.post('/bank', createBankController)
-server.put('/bank', updateBankController)
-server.delete('/bank', deleteBankController)
+server.get("/bank", viewBanksController);
+server.post("/bank", createBankController);
+server.put("/bank", updateBankController);
+server.delete("/bank", deleteBankController);
 
-server.listen(3000, () => console.log('all systems are go!');)
+server.listen(3000, () => console.log("all systems are go!"));
