927018c5c791a48c9bf4c6e09bac1eca7ee88d52
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-03-17 13:06:37 +0000

    created userModel

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+    },
+    email:{ 
+        type: String,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    account: {
+        type: Schema.Types.ObjectId,
+        ref: "Account",
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("User", UserSchema)
